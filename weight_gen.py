7574ed6aa586ad4e1932da76b46be4b0adac2cef
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-03 14:19:16 -0700

    Pull out generation of z from generate_weight

--- weight_gen.py
+++ weight_gen.py
@@ -3,20 +3,33 @@ from __future__ import print_function
 import numpy
 
 
-def weight(x, J, delta, sigma, rs):
-    z = rs.uniform(size=(len(x), len(x)))
+def weight(x, J, delta, sigma, z):
+    """
+    Generate a single block of connectivity matrix.
+    """
     return numpy.exp(-(x - x.T)**2/(2 * sigma**2)) * (J + delta * z)
 
 
-def generate_weight(N, J, delta, sigma, seed=None):
+def generate_weight(N, J, delta, sigma, z):
+    """
+    Generate 2N-by-2N connectivity matrix.
+    """
     J = numpy.asarray(J)
     delta = numpy.asarray(delta)
     sigma = numpy.asarray(sigma)
-    rs = numpy.random.RandomState(seed)
     x = numpy.linspace(-1, 1, N).reshape((1, -1))
     W = numpy.empty((2 * N, 2 * N))
-    W[:N, :N] = weight(x, J[0, 0], delta[0, 0], sigma[0, 0], rs)
-    W[N:, :N] = weight(x, J[1, 0], delta[1, 0], sigma[1, 0], rs)
-    W[:N, N:] = weight(x, -J[0, 1], delta[0, 1], sigma[0, 1], rs)
-    W[N:, N:] = weight(x, -J[1, 1], delta[1, 1], sigma[1, 1], rs)
+    W[:N, :N] = weight(x, J[0, 0], delta[0, 0], sigma[0, 0], z[:N, :N])
+    W[N:, :N] = weight(x, J[1, 0], delta[1, 0], sigma[1, 0], z[N:, :N])
+    W[:N, N:] = weight(x, -J[0, 1], delta[0, 1], sigma[0, 1], z[:N, N:])
+    W[N:, N:] = weight(x, -J[1, 1], delta[1, 1], sigma[1, 1], z[N:, N:])
     return W
+
+
+def generate_parameter(N, J, delta, sigma, seed=None):
+    """
+    Generate 2N-by-2N connectivity matrix and "latent" variable z.
+    """
+    rs = numpy.random.RandomState(seed)
+    z = rs.uniform(size=(2 * N, 2 * N))
+    return generate_weight(N, J, delta, sigma, z), z

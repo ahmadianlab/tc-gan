ae07ac53ac0aece0000954fc8f9ab120d23df367
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-03 17:01:20 -0700

    Use weight & generate_weight from weight_gen

--- nips_madness/gradient_expressions/SS_grad.py
+++ nips_madness/gradient_expressions/SS_grad.py
@@ -3,29 +3,10 @@ import theano.tensor as T
 import lasagne as L
 import numpy as np
 
+from ..weight_gen import weight, generate_weight
+
 #what I need is: to write an expression that takes a variable R and matrix W (ss response and weight matrix) and returns the gradient.
 #Now I need functions that take the parameters and compute Wgrads w.r.t. the parameters
-
-def weight(x, J, delta, sigma, z):
-    """
-    Generate a single block of connectivity matrix.
-    """
-    return np.exp(-(x - x.T)**2/(2 * sigma**2)) * (J + delta * z)
-
-def generate_weight(N, J, delta, sigma, z):
-    """
-    Generate 2N-by-2N connectivity matrix.
-    """
-    J = np.asarray(J)
-    delta = np.asarray(delta)
-    sigma = np.asarray(sigma)
-    x = np.linspace(-1, 1, N).reshape((1, -1))
-    W = np.empty((2 * N, 2 * N))
-    W[:N, :N] = weight(x, J[0, 0], delta[0, 0], sigma[0, 0], z[:N, :N])
-    W[N:, :N] = weight(x, J[1, 0], delta[1, 0], sigma[1, 0], z[N:, :N])
-    W[:N, N:] = weight(x, -J[0, 1], delta[0, 1], sigma[0, 1], z[:N, N:])
-    W[N:, N:] = weight(x, -J[1, 1], delta[1, 1], sigma[1, 1], z[N:, N:])
-    return W
 
 def weight_DJ(x, J, delta, sigma, z):
     '''

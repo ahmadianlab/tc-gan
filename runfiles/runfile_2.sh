fbc7a6caa737d6218333520e11a305017722a146
Gabriel <gabriel.barello@gmail.com>
2017-05-12 14:59:42 -0700

    changed runfiles

--- runfiles/runfile_2.sh
+++ runfiles/runfile_2.sh
@@ -4,4 +4,4 @@ cd research/NIPS_madness
 
 module load gcc/6.1
 
-python run_batch_GAN.py --loss CE --layers [128] --n_samples 20 --IO_type asym_linear
+./python run_batch_GAN.py --loss CE --layers [128] --n_samples 30 --IO_type asym_linear

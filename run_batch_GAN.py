9124fadbcd065694d65b49a98421331db8131107
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-14 16:37:27 -0700

    Remove unnecessary code

--- run_batch_GAN.py
+++ run_batch_GAN.py
@@ -394,19 +394,6 @@ def main(datapath, iterations, seed, gen_learn_rate, disc_learn_rate,
 
     try:
         os.makedirs('logfiles')
-    except IOError as err:
-        if err.errno != 17:
-            # If the error is "File exists" (errno=17) error, it means
-            # that the directory exists and it's fine to ignore the
-            # error.  It is slightly safer than checking existence of
-            # the directory since several processes may be creating it
-            # at the same time.  Maybe the error should be re-raised
-            # here but opening log file would fail anyway if there is
-            # something wrong.
-            print("!! Unexpected exception !!")
-            print(err)
-        else:
-            pass
     except OSError as err:
         if err.errno != 17:
             # If the error is "File exists" (errno=17) error, it means
@@ -418,8 +405,6 @@ def main(datapath, iterations, seed, gen_learn_rate, disc_learn_rate,
             # something wrong.
             print("!! Unexpected exception !!")
             print(err)
-        else:
-            pass
 
     # Clear data in old log files
     for filename in ["SSNGAN_log_{}.log",

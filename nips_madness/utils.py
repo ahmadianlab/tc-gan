b22d3b2ce5feab45a77b591a8fdd6f222ca61bb8
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-15 12:21:32 -0700

    More plotters

--- nips_madness/utils.py
+++ nips_madness/utils.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import os
 import subprocess
 import sys
@@ -22,3 +24,18 @@ def git_output(args):
         args,
         cwd=PROJECT_ROOT,
         universal_newlines=True)
+
+
+def make_progressbar(quiet=False, **kwds):
+    def dummy_bar(x):
+        return x
+
+    if quiet:
+        return dummy_bar
+    else:
+        try:
+            import progressbar
+        except ImportError:
+            return dummy_bar
+        else:
+            return progressbar.ProgressBar(**kwds)

a835ecdcd9214da3f09edbc5aaf1be6e2def2c3c
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-16 21:31:11 -0700

    Add test_io_sub_bound

--- nips_madness/tests/test_ssn.py
+++ nips_madness/tests/test_ssn.py
@@ -32,6 +32,29 @@ def test_io_power():
     check_io_fun('asym_power', libssnode.io_pow)
 
 
+def test_io_sub_bound(r0=ssnode.DEFAULT_PARAMS['rate_soft_bound'],
+                      r1=ssnode.DEFAULT_PARAMS['rate_hard_bound'],
+                      k=ssnode.DEFAULT_PARAMS['k'],
+                      n=ssnode.DEFAULT_PARAMS['n']):
+    common_kwargs = dict(
+        k=k, n=n, rate_soft_bound=r0, rate_hard_bound=r1)
+    v0 = ssnode.rate_to_volt(r0, k=k, n=n)
+    io_pow = ssnode.make_io_fun(io_type='asym_power', **common_kwargs)
+    io_atanh = ssnode.make_io_fun(io_type='asym_tanh', **common_kwargs)
+    io_alin = ssnode.make_io_fun(io_type='asym_linear', **common_kwargs)
+    assert 0 == io_pow(0) == io_atanh(0) == io_alin(0)
+    assert io_pow(r0 + 10) != io_atanh(r0 + 10)
+    assert io_pow(r0 + 10) != io_alin(r0 + 10)
+
+    for xs in [np.linspace(-10, 1000, 1000), [-0.0, 0, v0]]:
+        xs = np.array(xs)
+        ys_pow = io_pow(xs)
+        ys_atanh = io_pow(xs)
+        ys_alin = io_pow(xs)
+        np.testing.assert_allclose(ys_pow, ys_atanh, rtol=0, atol=1e-12)
+        np.testing.assert_allclose(ys_pow, ys_alin, rtol=0, atol=1e-12)
+
+
 def test_rate_to_volt(k=ssnode.DEFAULT_PARAMS['k'],
                       n=ssnode.DEFAULT_PARAMS['n']):
     xs = np.linspace(0, 1000, 1000)

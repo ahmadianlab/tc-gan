da64a51c10f9bd9cceff652820aea1bc65305c5d
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-17 21:29:31 -0700

    Add more options to benchmarks/rejection_rate.py

--- nips_madness/benchmarks/rejection_rate.py
+++ nips_madness/benchmarks/rejection_rate.py
@@ -6,13 +6,13 @@ from ..tests import test_dynamics  # ..to emit the warnings here
 del test_dynamics
 
 
-def run_benchmarks(NZ=30, **kwargs):
+def run_benchmarks(NZ, max_iter_list, **kwargs):
     print()
     print('{:<10} {:<7} {:<7} {:<10} {}'.format(
         'dt', '#rej', '%rej', 'time', 'codes'))
     dt_0 = 0.001
     max_iter_0 = 10000
-    for max_iter in [10000, 12500, 20000, 100000]:
+    for max_iter in max_iter_list:
         dt = dt_0 * max_iter_0 / max_iter
         with utils.StopWatch() as time:
             _zs, _xs, info = sample_fixed_points(
@@ -27,7 +27,16 @@ def run_benchmarks(NZ=30, **kwargs):
 
 
 def main(args=None):
-    run_benchmarks()
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--N', default=201, type=int)
+    parser.add_argument('--NZ', default=15, type=int)
+    parser.add_argument('--max-iter-list',
+                        default=[10000, 12500, 20000, 100000],
+                        type=lambda x: list(map(int, x.split(','))),
+                        help='dt is computed from this.')
+    ns = parser.parse_args(args)
+    run_benchmarks(**vars(ns))
 
 
 if __name__ == '__main__':

b72886534619cfe860423406ce6bf9a4ddeebb61
Takafumi Arakaki <aka.tkf@gmail.com>
2017-05-26 14:12:28 -0700

    Treat track_net_identity in plot_tunings

--- nips_madness/plot_tunings.py
+++ nips_madness/plot_tunings.py
@@ -1,19 +1,33 @@
 from __future__ import print_function, division
 
 from matplotlib import pyplot
+import numpy as np
 
 from .ssnode import sample_tuning_curves, DEFAULT_PARAMS
 
 
 def plot_tunings(bandwidths=DEFAULT_PARAMS['bandwidths'],
+                 sample_sites=3,
+                 track_net_identity=False,
                  linewidth=0.2, ylim=(None, None),
                  **sample_kwargs):
-    sample_kwargs.update(bandwidths=bandwidths)
-    tunings, sample = sample_tuning_curves(**sample_kwargs)
+    tunings, sample = sample_tuning_curves(
+        bandwidths=bandwidths,
+        sample_sites=sample_sites,
+        track_net_identity=track_net_identity,
+        **sample_kwargs)
+
+    xlabel = 'Bandwidths'
+    if track_net_identity:
+        bandwidths = np.asarray(bandwidths)
+        bandwidths = np.concatenate([
+            bandwidths + i for i in range(sample_sites)
+        ])
+        xlabel = 'Bandwidths (shifted for different sites)'
 
     fig, ax = pyplot.subplots()
     ax.plot(bandwidths, tunings, color='C0', linewidth=linewidth)
-    ax.set_xlabel('Bandwidths')
+    ax.set_xlabel(xlabel)
     ax.set_ylabel('Rates')
     ax.set_ylim(*ylim)
 

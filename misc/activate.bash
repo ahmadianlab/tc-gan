a3ada2b72394b864f17571d333c02f2de7ce5567
Takafumi Arakaki <aka.tkf@gmail.com>
2017-09-30 23:22:22 -0700

    Don't use conda's activation script in with-env
    It doesn't work well on NFS.

--- misc/activate.bash
+++ misc/activate.bash
@@ -1,3 +1,11 @@
 thisdir="$(dirname "${BASH_SOURCE[0]}")"
 source "$thisdir/rc/rc.sh"
-source "$thisdir/../env/bin/activate" "$thisdir/../env"
+
+PATH="$thisdir/../env/bin:$PATH"
+# In principle, this is better done via env/bin/activate like this:
+#
+#     source "$thisdir/../env/bin/activate" "$thisdir/../env"
+#
+# However, as conda tries to symlink stuff during activation, it's not
+# a good idea to use it in a script to be called in parallel (see,
+# e.g., https://github.com/conda/conda/issues/3001).

b71044c5a4846fe242972de9d83c6f0a4abc381a
Takafumi Arakaki <aka.tkf@gmail.com>
2017-10-02 17:32:29 -0700

    Add test_save_on_error

--- nips_madness/tests/test_lasagne_param_file.py
+++ nips_madness/tests/test_lasagne_param_file.py
@@ -1,5 +1,6 @@
 import lasagne
 import numpy
+import pytest
 
 from .. import lasagne_param_file
 
@@ -13,3 +14,27 @@ def test_dump_then_load(tmpdir):
     assert isinstance(values, list)
     desired = lasagne.layers.get_all_param_values(l1)
     numpy.testing.assert_equal(values, desired)
+
+
+def test_save_on_error(tmpdir):
+    l0 = lasagne.layers.InputLayer((2, 3))
+    l1 = lasagne.layers.DenseLayer(l0, 5)
+    param_path = str(tmpdir.join('param_file.npz'))
+
+    values = [arr.copy() for arr in lasagne.layers.get_all_param_values(l1)]
+
+    class MyException(Exception):
+        pass
+
+    try:
+        with lasagne_param_file.save_on_error(l1, param_path):
+            newvalues = [arr + 1 for arr in values]
+            lasagne.layers.set_all_param_values(l1, newvalues)
+            raise MyException
+    except MyException:
+        pass
+
+    stored = lasagne_param_file.load(param_path)
+    numpy.testing.assert_equal(stored, values)
+    with pytest.raises(AssertionError):
+        numpy.testing.assert_equal(stored, newvalues)

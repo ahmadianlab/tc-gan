Fix DiscParamStatsRecorder for layer normalization

nips_madness/recorders.py
@@ -167,7 +167,8 @@ class DiscParamStatsRecorder(BaseRecorder):
     def record(self, gen_step, disc_step):
         nnorms = [
             np.linalg.norm(arr.flatten()) / arr.size
-            for arr in lasagne.layers.get_all_param_values(self.discriminator)
+            for arr in lasagne.layers.get_all_param_values(self.discriminator,
+                                                           trainable=True)
         ]
         # TODO: implement it using Theano function
         self._saverow([gen_step, disc_step] + nnorms)
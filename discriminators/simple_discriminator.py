cba0555f0941c33711504c2c9d191d12bfa3400e
Takafumi Arakaki <aka.tkf@gmail.com>
2017-10-16 13:00:26 -0700

    Add dummy discriminators/simple_discriminator.py

---
+++ discriminators/simple_discriminator.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.warn('Use nips_madness.networks.simple_discriminator instead of {}'
+              .format(__name__))
+
+from nips_madness.networks.simple_discriminator import *

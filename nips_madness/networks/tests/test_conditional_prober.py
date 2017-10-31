b1d42abfd0002ee484245f63ede2e65dcea79f56
Takafumi Arakaki <aka.tkf@gmail.com>
2017-10-31 15:39:17 -0700

    Add test_conditional_prober.py

---
+++ nips_madness/networks/tests/test_conditional_prober.py
@@ -0,0 +1,72 @@
+from unittest import mock
+
+import numpy as np
+import theano
+
+from ..cwgan import ConditionalProber
+
+
+def mock_model():
+    model = mock.Mock()
+    model.num_sites = 201
+    model.time_avg = theano.tensor.tensor3('time_avg')
+    model.stimulator.contrasts = theano.tensor.matrix('contrasts')
+    return model
+
+
+def test_conditional_probes():
+    prober = ConditionalProber(mock_model())
+
+    probe_offsets = np.array([-1, -0.5, 0, 0.5, 1] * 2,
+                             dtype=theano.config.floatX)
+    cell_types = np.array([0] * 5 + [1] * 5, dtype='uint16')
+
+    probes = prober.probes.eval({
+        prober.probe_offsets: probe_offsets,
+        prober.cell_types: cell_types,
+    })
+    desired = [0, 50, 100, 150, 200,
+               201, 251, 301, 351, 401]
+    np.testing.assert_equal(probes, desired)
+
+
+def test_conditional_tuning_curve():
+    prober = ConditionalProber(mock_model())
+
+    num_models = 2
+    num_tcdom = 3
+    num_neurons = prober.model.num_sites * 2
+    probe_offsets = np.array([-1, -0.5, 0, 0.5, 1] * 2,
+                             dtype=theano.config.floatX)
+    cell_types = np.array([0] * 5 + [1] * 5, dtype='uint16')
+    model_ids = np.array([0, 1] * 5, dtype='uint16')
+    shape = (num_models, num_tcdom, num_neurons)
+    time_avg = np.arange(np.prod(shape),
+                         dtype=theano.config.floatX).reshape(shape)
+
+    assert probe_offsets.shape == cell_types.shape == model_ids.shape
+    assert all(model_ids < num_models)
+
+    tuning_curve = prober.tuning_curve.eval({
+        prober.probe_offsets: probe_offsets,
+        prober.cell_types: cell_types,
+        prober.model_ids: model_ids,
+        prober.model.time_avg: time_avg,
+    })
+    assert tuning_curve.shape == (len(probe_offsets), num_tcdom)
+
+    # Following `desired` value was copied from `tuning_curve` to
+    # "quench" the implementation.
+    desired = [
+        [0,      402,   804],
+        [1256,  1658,  2060],
+        [100,    502,   904],
+        [1356,  1758,  2160],
+        [200,    602,  1004],
+        [1407,  1809,  2211],
+        [251,    653,  1055],
+        [1507,  1909,  2311],
+        [351,    753,  1155],
+        [1607,  2009,  2411],
+    ]
+    np.testing.assert_equal(tuning_curve, desired)

ab2b76f97bae4bb151e3cb09aea229efb2159d48
Takafumi Arakaki <aka.tkf@gmail.com>
2017-10-31 17:57:10 -0700

    Add test_conditional_minibatch_prober_contrasts

--- nips_madness/networks/tests/test_conditional_minibatch.py
+++ nips_madness/networks/tests/test_conditional_minibatch.py
@@ -3,7 +3,8 @@ from types import SimpleNamespace
 import numpy as np
 import pytest
 
-from ..cwgan import ConditionalMinibatch
+from ..cwgan import ConditionalMinibatch, ConditionalProber
+from .test_conditional_prober import mock_model
 
 
 def arangemd(shape, **kwargs):
@@ -16,6 +17,8 @@ def make_conditional_minibatch(num_models=2, probes_per_model=3,
     conditions_md = arangemd((3, num_models, probes_per_model))
     bandwidths = np.arange(num_bandwidths)
     contrasts = np.arange(num_models)
+    conditions_md[0] = contrasts.reshape(-1, 1)
+    # See: [[../cwgan.py::select_minibatch]]
     return ConditionalMinibatch(tc_md, conditions_md, bandwidths, contrasts)
 
 
@@ -45,3 +48,27 @@ def test_conditional_minibatch(num_models, probes_per_model, num_bandwidths):
     stimulator_shape = (num_models, num_bandwidths)
     assert gen_kwargs.stimulator_bandwidths.shape == stimulator_shape
     assert gen_kwargs.stimulator_contrasts.shape == stimulator_shape
+
+
+@pytest.mark.parametrize('num_models, probes_per_model', [
+    (1, 3),
+    (2, 3),
+    (5, 7),
+    (8, 64),
+])
+def test_conditional_minibatch_prober_contrasts(num_models, probes_per_model):
+    batch = make_conditional_minibatch(num_models, probes_per_model)
+    prober = ConditionalProber(mock_model())
+
+    gen_kwargs = SimpleNamespace(**batch.gen_kwargs)
+    prober_contrasts = prober.contrasts.eval({
+        prober.model.stimulator.contrasts: gen_kwargs.stimulator_contrasts,
+        prober.model_ids: batch.model_ids,
+    })
+
+    batch_contrasts, _probe_offsets, _cell_types = batch.conditions.T
+    # See:
+    # * [[../cwgan.py::get_output]]
+    # * [[../cwgan.py::gen_kwargs]]
+
+    np.testing.assert_equal(prober_contrasts, batch_contrasts)

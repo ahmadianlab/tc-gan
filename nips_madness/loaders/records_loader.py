1a50998cda905cb387699bc5cf59a0d9304405a4
Takafumi Arakaki <aka.tkf@gmail.com>
2017-12-22 19:49:48 -0800

    Start writing new loaders

---
+++ nips_madness/loaders/records_loader.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import json
+
+from ..analyzers.loader import guess_run_module
+from ..utils import cached_property
+from .datastore_loader import get_datastore
+from .run_configs import get_run_config
+
+
+def cached_record(name):
+    def get(self, name):
+        return self.datastore.load(name)
+    get.__name__ = name
+    return cached_property(get)
+
+
+class BaseRecords(object):
+
+    def __init__(self, datastore, info, rc):
+        self.datastore = datastore
+        self.info = info
+        self.rc = rc
+
+    learning = cached_record('learning')
+    generator = cached_record('generator')
+
+    @classmethod
+    def from_info(cls, info, datastore_path):
+        rc = get_run_config(info)
+        datastore = get_datastore(datastore_path, info)
+        return cls(datastore, info, rc)
+
+
+class GANRecords(BaseRecords):
+
+    disc_param_stats = cached_record('disc_param_stats')
+    disc_learning = cached_record('disc_learning')
+
+
+class ConditionalGANRecords(GANRecords):
+
+    tc_stats = cached_record('tc_stats')
+
+
+class MomentMatchingRecords(BaseRecords):
+
+    gen_moments = cached_record('gen_moments')
+
+
+def get_datastore_path(path):
+    datastore_path = Path(path)
+    if not datastore_path.is_dir():
+        datastore_path = datastore_path.parent
+    return datastore_path
+
+
+def load_info(path):
+    datastore_path = get_datastore_path(path)
+    with open(str(datastore_path.joinpath('info.json'))) as file:
+        info = json.load(file)
+    info['datastore_path'] = datastore_path
+    return info
+
+
+module_records_map = {
+    'bptt_wgan': GANRecords,
+    'bptt_cwgan': ConditionalGANRecords,
+    'bptt_moments': MomentMatchingRecords,
+    # Legacy GAN modules.  Not well supported, but why not let them be
+    # loaded:
+    'gan': GANRecords,
+    'cgan': GANRecords,
+}
+
+
+def load_records(path):
+    datastore_path = get_datastore_path(path)
+    info = load_info(datastore_path)
+    run_module = guess_run_module(info)
+    return module_records_map[run_module].from_info(info, datastore_path)
---
+++ nips_madness/loaders/run_configs.py
@@ -0,0 +1,202 @@
+import abc
+
+import numpy as np
+
+
+class AbstractGANRunConfig(abc.ABC):
+
+    @classmethod
+    def from_info(cls, info):
+        return cls(info['run_config'])
+
+    def __init__(self, run_config):
+        self.dict = run_config
+
+    def __getattr__(self, name):
+        try:
+            return self.dict[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    @property
+    def datasize(self):
+        return self.dict['truth_size']
+
+    @property
+    def n_bandwidths(self):
+        return len(self.bandwidths)
+
+    @property
+    def n_contrasts(self):
+        return len(self.contrasts)
+
+    # @property
+    # def n_stim(self):
+    #     return self.n_bandwidths * self.n_contrasts
+
+    def gen_step_to_epoch(self, gen_step):
+        disc_updates = self.gen_step_to_disc_updates(gen_step)
+        return disc_updates * self.batchsize / self.datasize
+
+    def gen_step_to_disc_updates(self, gen_step):
+        if self.is_WGAN:
+            WGAN_n_critic0 = self.critic_iters_init
+            WGAN_n_critic = self.critic_iters
+            return WGAN_n_critic0 + WGAN_n_critic * gen_step
+        else:
+            return gen_step + 1
+
+    def epoch_to_gen_step(self, epoch):
+        return self.disc_updates_to_gen_step(self.epoch_to_disc_updates(epoch))
+
+    def disc_updates_to_gen_step(self, disc_updates):
+        if self.is_WGAN:
+            WGAN_n_critic0 = self.critic_iters_init
+            WGAN_n_critic = self.critic_iters
+            return (disc_updates - WGAN_n_critic0) / WGAN_n_critic
+        else:
+            return disc_updates - 1
+
+    def epoch_to_disc_updates(self, epoch):
+        return epoch * self.datasize / self.batchsize
+
+
+class LegacyGANRunConfig(AbstractGANRunConfig):
+
+    @property
+    def track_offset_identity(self):
+        return self.dict.get('track_offset_identity', False)
+
+    @property
+    def include_inhibitory_neurons(self):
+        return self.dict.get('include_inhibitory_neurons', False)
+
+    @property
+    def n_sample_sites(self):
+        try:
+            sample_sites = self.dict['sample_sites']
+        except KeyError:
+            return 1
+        if self.include_inhibitory_neurons:
+            return len(sample_sites) * 2
+        else:
+            return len(sample_sites)
+
+    @property
+    def n_bandwidths(self):
+        try:
+            return len(self.dict['bandwidths'])
+        except KeyError:
+            pass
+        try:
+            return self.dict['n_bandwidths']
+        except KeyError:
+            return 8
+
+    @property
+    def contrasts(self):
+        return self.dict.get('contrast', [20])
+
+    @property
+    def contrast(self):
+        raise AttributeError("Accessing `contrast` not allowed."
+                             " Use `contrasts`.")
+
+    @property
+    def bandwidths(self):
+        try:
+            return self.dict['bandwidths']
+        except KeyError:
+            pass
+
+        # Loading old logfiles:
+        n = self.n_bandwidths
+        if n == 8:
+            return np.array([0, 0.0625, 0.125, 0.1875, 0.25, 0.5, 0.75, 1])
+        elif n == 5:
+            return np.array([0.0625, 0.125, 0.25, 0.5, 0.75])
+        elif n == 4:
+            return np.array([0.0625, 0.125, 0.25, 0.75])
+        else:
+            raise ValueError("Unknown n_bandwidth: {}".format(n))
+
+    @property
+    def batchsize(self):
+        return self.dict['n_samples']
+
+    @property
+    def critic_iters(self):
+        return self.dict.get('WGAN_n_critic', 5)
+
+    @property
+    def critic_iters_init(self):
+        return self.dict.get('WGAN_n_critic0', 50)
+
+    @property
+    def gan_type(self):
+        loss = self.dict['loss']
+        try:
+            return {
+                'CE': 'RGAN',
+                'LS': 'LSGAN',
+                'WD': 'WGAN',
+            }[loss]
+        except KeyError:
+            pass
+        return '{}-GAN'.format(loss)
+
+    @property
+    def is_WGAN(self):
+        return self.gan_type in ('cWGAN', 'WGAN')
+
+
+class AbstractWGANRunConfig(AbstractGANRunConfig):
+    track_offset_identity = True
+    is_WGAN = True
+
+
+class BPTTWGANRunConfig(AbstractWGANRunConfig):
+    gan_type = 'WGAN'
+
+
+class BPTTcWGANRunConfig(AbstractWGANRunConfig):
+    gan_type = 'cWGAN'
+
+    @property
+    def batchsize(self):
+        run_config = self.dict
+        return run_config['num_models'] * run_config['probes_per_model']
+
+    @property
+    def datasize(self):
+        run_config = self.dict
+        truth_size = run_config['truth_size']
+        try:
+            num_probes = len(run_config['norm_probes'])
+        except KeyError:
+            num_probes = len(run_config['probe_offsets'])
+        num_contrasts = len(run_config['contrasts'])
+        if run_config['include_inhibitory_neurons']:
+            # coeff = (0.8 + 0.2) / (0.8 + 0.8)
+            coeff = 1 / (2 * self.e_ratio)
+            return truth_size * num_probes * num_contrasts * coeff
+        else:
+            return truth_size * num_probes * num_contrasts
+
+    @property
+    def e_ratio(self):
+        return self.dict.get('e_ratio', 0.8)
+
+
+module_class_map = {
+    'bptt_wgan': BPTTWGANRunConfig,
+    'bptt_cwgan': BPTTcWGANRunConfig,
+}
+
+
+def get_run_config(info):
+    from .loader import guess_run_module
+    run_module = guess_run_module(info)
+    return module_class_map.get(
+        run_module, LegacyGANRunConfig,
+    ).from_info(info)

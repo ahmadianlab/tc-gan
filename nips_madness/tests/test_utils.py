4aae36afb307ef84697333bb6e126c7abef01e3d
Takafumi Arakaki <aka.tkf@gmail.com>
2017-12-24 07:35:01 -0800

    Fix cpu_count for Slurm
    It didn't work when select/linear plugin was used (e.g., in stampede2).

--- nips_madness/tests/test_utils.py
+++ nips_madness/tests/test_utils.py
@@ -13,7 +13,8 @@ def take(n, iterable):
 
 
 def test_cpu_count():
-    assert cpu_count(_environ={'SLURM_CPUS_ON_NODE': '-3'}) == -3
+    assert cpu_count(_environ={'SLURM_CPUS_PER_TASK': '-3'}) == -3
+    assert cpu_count(_environ={'SLURM_JOB_ID': None}) == 1
     assert cpu_count(_environ={'PBS_NUM_PPN': '-5'}) == -5
     assert cpu_count(_environ={}) == multiprocessing.cpu_count()
 
--- nips_madness/utils.py
+++ nips_madness/utils.py
@@ -164,14 +164,29 @@ def cpu_count(_environ=os.environ):
     """cpu_count() -> int
     Return available number of CPUs; Slurm/PBS-aware version.
     """
+
     try:
-        return int(_environ['SLURM_CPUS_ON_NODE'])
+        # Note: Using SLURM_CPUS_PER_TASK here instead of
+        # SLURM_CPUS_ON_NODE and SLURM_JOB_CPUS_PER_NODE as they may
+        # return just the total number of CPUs on the node, depending
+        # on the plugin used (select/linear vs select/cons_res; see
+        # document of SLURM_JOB_CPUS_PER_NODE in man sbatch and
+        # SLURM_CPUS_ON_NODE in man srun).
+        return int(_environ['SLURM_CPUS_PER_TASK'])
     except (KeyError, ValueError):
         pass
+    if 'SLURM_JOB_ID' in _environ:
+        # SLURM_CPUS_PER_TASK is specified only when --cpus-per-task
+        # option is specified; otherwise, Slurm allocates one
+        # processor per task (see document of --cpus-per-task in man
+        # sbatch).
+        return 1
+
     try:
         return int(_environ['PBS_NUM_PPN'])
     except (KeyError, ValueError):
         pass
+
     return multiprocessing.cpu_count()
 
 

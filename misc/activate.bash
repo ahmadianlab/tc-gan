baabebfae564d5f34f11e0977c0da3df5165bdf2
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-15 20:45:50 -0800

    Set MKL_THREADING_LAYER=GNU always

--- misc/activate.bash
+++ misc/activate.bash
@@ -17,3 +17,7 @@ export LD_LIBRARY_PATH="$env/lib:$LD_LIBRARY_PATH"
 # However, as conda tries to symlink stuff during activation, it's not
 # a good idea to use it in a script to be called in parallel (see,
 # e.g., https://github.com/conda/conda/issues/3001).
+
+# Just following the instruction (via RuntimeError) from Theano.
+# https://github.com/Theano/Theano/issues/6499#issuecomment-342160463
+export MKL_THREADING_LAYER=GNU

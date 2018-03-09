388fad28f714ae994fe14eb12221ea49d13ef3ae
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-08 22:16:11 -0800

    Support rolling SNR/CV/Fano

--- nips_madness/utils/stats.py
+++ nips_madness/utils/stats.py
@@ -1,14 +1,32 @@
 import numpy
 
 
+def getstat(thing, stat):
+    if stat == 'snr':  # signal-to-noise ratio
+        m = thing.mean()
+        s = thing.std()
+        return m / s
+    elif stat == 'cv':
+        m = thing.mean()
+        s = thing.std()
+        return s / m
+    elif stat == 'fano':
+        m = thing.mean()
+        v = thing.var()
+        return v / m
+
+    return getattr(thing, stat)()
+
+
 def rolling_stat(stat, x, window):
     """
     Calculate rolling statistics `stat` of `x` with sliding `window`.
 
     Parameters
     ----------
-    stat : 'mean', 'median', ...
-        Name (`str`) of the rolling statistics supported by pandas.
+    stat : 'snr', 'cv', 'fano', 'mean', 'median', ...
+        Name (`str`) of the rolling statistics supported by pandas
+        and `getstat` (e.g., `'snr'`).
         https://pandas.pydata.org/pandas-docs/stable/api.html#standard-moving-window-functions
     x : array-like
         1D or 2D numpy array or anything convertable to it.
@@ -32,5 +50,5 @@ def rolling_stat(stat, x, window):
         z = pandas.Series(x)
     else:
         z = pandas.DataFrame(x)
-    y = getattr(z.rolling(window=window), stat)().values
+    y = getstat(z.rolling(window=window), stat).values
     return y[window - 1:]
